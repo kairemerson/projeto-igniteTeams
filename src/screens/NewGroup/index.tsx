93572d350b97218bc2973ca465d672d65800536e
Remerson Monteiro <kairemerson@gmail.com>
2025-04-17 20:15:58 -0300

    feat: created screens NewGroups

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,21 @@
+import { Header } from "@components/Header";
+import { Content, Icon, NewGroupContainer } from "./styles";
+import { Highlight } from "@components/Highlight";
+import { Button } from "@components/Button";
+import { Input } from "@components/Input";
+
+
+export function NewGroup() {
+    return (
+        <NewGroupContainer>
+            <Header showBackButton/>
+
+            <Content>
+                <Icon/>
+                <Highlight title="Nova turma" subtitle="crie a turma para adicionar as pessoas"/>
+                <Input placeholder="Nome da turma"/>
+                <Button title="Criar" style={{marginTop: 20}}/>
+            </Content>
+        </NewGroupContainer>
+    )
+}
