0d9085b88222dcf98d6de4b6d0d11b2134ffc949
Remerson Monteiro <kairemerson@gmail.com>
2025-04-16 22:11:30 -0300

    feat: add fonts and loadder while fonts load

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
+import {StatusBar} from "react-native"
 import {ThemeProvider} from "styled-components"
-import { Groups } from '@screens/Groups';
 import {theme} from "./src/theme";
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from "@expo-google-fonts/roboto"
+
+import { Groups } from '@screens/Groups';
+import { Loading } from "@components/Loading";
 
 export default function App() {
+
+  const [fontsLoader] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups/>
-
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/> 
+        {fontsLoader ? <Groups/> : <Loading/>}
     </ThemeProvider>
   );
 }
