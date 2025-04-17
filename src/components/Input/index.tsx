83390291a8f8afd130baf520026598dfdeea2c7e
Remerson Monteiro <kairemerson@gmail.com>
2025-04-17 20:14:40 -0300

    feat: created component input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,15 @@
+import { TextInputProps } from "react-native";
+import { InputContainer } from "./styles";
+import { useTheme } from "styled-components/native";
+
+
+
+export function Input({...rest}: TextInputProps) {
+    const {COLORS} = useTheme()
+
+    return(
+        <InputContainer placeholderTextColor={COLORS.GRAY_300} {...rest} >
+        
+        </InputContainer>
+    )
+}
