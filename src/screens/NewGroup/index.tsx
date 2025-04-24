f3e8b2a2e325bacb5937f9cd5f9439caf7689283
Remerson Monteiro <kairemerson@gmail.com>
2025-04-24 19:42:44 -0300

    feat: created component NewGroups

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,11 +1,39 @@
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 import { Header } from "@components/Header";
 import { Content, Icon, NewGroupContainer } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
+import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppErros";
+import { Alert } from "react-native";
 
 
 export function NewGroup() {
+
+    const [group, setGroup] = useState("")
+
+    const navigation = useNavigation()
+
+    async function handleNew() {
+        try {
+            if(group.trim().length === 0){
+                return Alert.alert("Novo grupo", "Informe o nome da turma")
+            }
+
+            await groupCreate(group)
+            navigation.navigate("players", {group: group})
+            
+        } catch (error) {
+            if(error instanceof AppError){
+                Alert.alert("Novo grupo", error.message)
+            }else{
+                Alert.alert("Novo grupo", "Não foi possível criar novo grupo")
+            }
+        }
+    }
+
     return (
         <NewGroupContainer>
             <Header showBackButton/>
@@ -13,8 +41,8 @@ export function NewGroup() {
             <Content>
                 <Icon/>
                 <Highlight title="Nova turma" subtitle="crie a turma para adicionar as pessoas"/>
-                <Input placeholder="Nome da turma"/>
-                <Button title="Criar" style={{marginTop: 20}}/>
+                <Input placeholder="Nome da turma" onChangeText={setGroup}/>
+                <Button title="Criar" style={{marginTop: 20}} onPress={handleNew}/>
             </Content>
         </NewGroupContainer>
     )
