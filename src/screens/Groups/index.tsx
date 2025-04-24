2ee9599f66e2018d8b6b87a08719abf476c9b2a3
Remerson Monteiro <kairemerson@gmail.com>
2025-04-24 19:42:32 -0300

    feat: created component Groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,44 @@
+import { useCallback, useEffect, useState } from "react";
+import { Alert, FlatList } from "react-native";
+import {useFocusEffect, useNavigation} from "@react-navigation/native"
+
 import { Header } from "@components/Header";
 import { Container} from "./styles";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
-import { useState } from "react";
-import { FlatList } from "react-native";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
+import { groupsGetAll } from "@storage/group/groupsGetAll";
 
 
 export function Groups(){
 
-    const [groups, setGroups] = useState([])
+    const [groups, setGroups] = useState<string[]>([])
+
+    const navigation = useNavigation()
+
+    function handleNewGroup() {
+        navigation.navigate("new")
+    }
+
+    async function fetchGroups() {
+        try {
+            const data = await groupsGetAll()
+            setGroups(data)
+        } catch (error) {
+            console.log(error);
+            Alert.alert("Turmas", "Não foi possível carregar as turmas")
+        }
+    }
+
+    function handleOpenGroup(group: string) {
+        navigation.navigate("players", {group})
+    }
+
+    useFocusEffect(useCallback(()=> {
+        fetchGroups()
+    }, []))
+
     return (
         <Container>
             <Header/>
@@ -20,14 +48,17 @@ export function Groups(){
                 data={groups}
                 keyExtractor={item => item}
                 renderItem={({item})=>(
-                    <GroupCard title={item}/>
+                    <GroupCard 
+                        title={item}
+                        onPress={()=> handleOpenGroup(item)}
+                    />
 
                 )}
                 contentContainerStyle={groups.length === 0 && {flex: 1}}
                 ListEmptyComponent={()=> <ListEmpty message="Que tal cadastrar a primeira turma?"/>}
             />
 
-            <Button title="Criar nova turma"/>
+            <Button title="Criar nova turma" onPress={handleNewGroup}/>
         </Container>
     )
 }
